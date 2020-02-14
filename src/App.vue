f43f8c1dd3c5a21dd79e59a9d40d0dfdbcec624e
marknaja5678 <57784134+marknaja5678@users.noreply.github.com>
2020-02-14 12:01:02 +0700

    up

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+
+html, body {
+  width: 100%;
+  height: 100%;
+  background-color: aquamarine;
+  overflow: hidden;
+}
+
+</style>
